642527bc15d883a23aaa48a7a33a7d92998c3170
enlf123 <33775481+enlf123@users.noreply.github.com>
2019-03-06 16:36:31 +0900

    Add files via upload

---
+++ Encoder/Magnetic/read_sensor.ino
@@ -0,0 +1,34 @@
+int sensorValue = 0;
+int sensorValue_old = 0;
+int cnt = 0;
+double ang = 0.0;
+double preset[5];
+double avg_preset = 0;
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+  for(int i=0;i<5;i++){
+  sensorValue = analogRead(A0);
+  if (sensorValue-sensorValue_old > 500) cnt = cnt - 1;
+  else if (sensorValue-sensorValue_old < -500) cnt = cnt + 1;
+  ang = (double)cnt*360.0 + (double)sensorValue*360.0/1023.0;
+  sensorValue_old = sensorValue; //이전 센서 값을 지금 읽은 값으로 업데이트
+  preset[i] =ang;
+  }
+  avg_preset = (preset[0]+preset[1]+preset[2]+preset[3]+preset[4])/5;
+  Serial.println(avg_preset);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+//   read the input on analog pin 0:
+  sensorValue = analogRead(A0);
+  if (sensorValue-sensorValue_old > 500) cnt = cnt - 1;
+  else if (sensorValue-sensorValue_old < -500) cnt = cnt + 1;
+  ang = (double)cnt*360.0 + (double)sensorValue*360.0/1023.0;
+
+  sensorValue_old = sensorValue; //이전 센서 값을 지금 읽은 값으로 업데이트
+ // print out the ang:
+  Serial.println(ang-avg_preset);
+  delay(50);
+}
