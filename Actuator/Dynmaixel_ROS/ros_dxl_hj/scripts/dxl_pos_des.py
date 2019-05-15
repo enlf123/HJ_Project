64f516c1e6ac5f4dbc920448a25a79b33d672cf8
enlf123 <33775481+enlf123@users.noreply.github.com>
2019-05-15 19:22:15 +0900

    Add files via upload

---
+++ Actuator/Dynmaixel_ROS/ros_dxl_hj/scripts/dxl_pos_des.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+## Simple talker demo that published std_msgs/Strings messages
+## to the 'chatter' topic
+
+import rospy
+import math
+from dynamixel_sdk import *
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+from std_msgs.msg import Int32
+
+# Control table address
+ADDR_PRO_RETURN_TIME	    = 9
+ADDR_PRO_OPERATING_MODE	    = 11
+ADDR_PRO_HOMING_OFFSET 	    = 20
+ADDR_PRO_MAX_POS 	    = 48
+ADDR_PRO_MIN_POS 	    = 52
+ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
+ADDR_PRO_PROFILE_VELOCITY   = 112
+ADDR_PRO_GOAL_POSITION      = 116
+ADDR_PRO_PRESENT_POSITION   = 132
+
+# Data Byte Length
+LEN_PRO_GOAL_POSITION       = 4
+LEN_PRO_PRESENT_POSITION    = 4
+LEN_PRO_PROFILE_VELOCITY    = 4
+
+
+# Protocol version
+PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
+
+# Default setting
+DXL_ID                      = 2               # Dynamixel#1 ID : 2
+BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
+DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+# ** setserial /dev/ttyUSB0 low_latency - if you want more communication speed, write this in terminal   https://github.com/ROBOTIS-GIT/DynamixelSDK/issues/80
+
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+def dxl_pos_read():
+    portHandler.openPort()
+    portHandler.setBaudRate(BAUDRATE)
+
+    pub = rospy.Publisher('dxl_pos', Int32, queue_size=1)
+    rospy.init_node('dxl_pos_read', anonymous=True)
+    rate = rospy.Rate(500) # 100hz
+    cnt = 0.0;
+    while not rospy.is_shutdown():
+#        portHandler.openPort()
+#        portHandler.setBaudRate(BAUDRATE)
+        dxl_goal_position = 2000+int(2000*math.sin(cnt/300.0))
+        print("%s" % dxl_goal_position)
+    # Write Goal Position
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, dxl_goal_position)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+       
+        
+        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_PRESENT_POSITION)
+        if dxl_comm_result != COMM_SUCCESS:
+            print "%s" % packetHandler.getTxRxResult(dxl_comm_result)
+        elif dxl_error != 0:
+            print "%s" % packetHandler.getRxPacketError(dxl_error)
+        rospy.loginfo(dxl_present_position)
+        pub.publish(dxl_present_position)
+#        portHandler.closePort()
+        rate.sleep()
+        cnt = cnt+1.0
+
+
+if __name__ == '__main__':
+    try:
+        dxl_pos_read()
+    except rospy.ROSInterruptException:
+        pass
