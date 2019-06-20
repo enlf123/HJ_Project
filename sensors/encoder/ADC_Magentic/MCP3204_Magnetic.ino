ef30d9dd198275ba773e5c6c83418cb75b41a0b9
enlf123 <33775481+enlf123@users.noreply.github.com>
2019-06-20 22:00:00 +0900

    Update MCP3204_Magnetic.ino

--- sensors/encoder/ADC_Magentic/MCP3204_Magnetic.ino
+++ sensors/encoder/ADC_Magentic/MCP3204_Magnetic.ino
@@ -1,45 +1,55 @@
 // MCP3204 ADC(12bit) Read using Arduino Mega
 
-#define CS 10
-#define MOSI 11
-#define MISO  12
-#define CLK 13
 
-int ch = 1; // select ch 0~
+// MCP3204 ADC(12bit) Read using Arduino Mega
+
+#define CS 10 
+#define DI 11
+#define DO 12
+#define SPICLOCK  13//Clock 
+
+int ch = 0; // select ch 0~
 int readvalue; 
 
 double ang =0.0;
 double preset[5];
 double avg_preset = 0;
+unsigned long pre, cur;
+
 
 void setup(){ 
  //set pin modes 
  pinMode(CS, OUTPUT); 
- pinMode(MOSI, OUTPUT); 
- pinMode(MISO, INPUT); 
+ pinMode(DI, OUTPUT); 
+ pinMode(DO, INPUT); 
  pinMode(SPICLOCK, OUTPUT); 
  //disable device to start with 
  digitalWrite(CS,HIGH); 
- digitalWrite(MOSI,LOW); 
- digitalWrite(CLK,LOW); 
+ digitalWrite(DI,LOW); 
+ digitalWrite(SPICLOCK,LOW); 
 
  Serial.begin(115200);
 
  // Initialization for removing bias
  for(int i=0;i<5;i++){
-   readvalue = read_adc(1); 
+   readvalue = read_adc(ch); 
  ang = (double)readvalue*360/4095.0;
  preset[i] =ang;
  }
  avg_preset = (preset[0]+preset[1]+preset[2]+preset[3]+preset[4])/5;
+ pre = micros();
 } 
 
 // reading absoulte angle through ezEncoder(magnetic angle sensor)
 void loop() { 
+  cur = micros();
+  if (cur-pre > 2000){
  readvalue = read_adc(ch); 
  ang = (double)readvalue*360/4095.0;
- Serial.println(ang-avg_preset); 
- delay(10); 
+ //Serial.println(ang-avg_preset); 
+ Serial.println(cur-pre);
+ pre = micros();
+  }
 } 
 
 //function
@@ -48,28 +58,28 @@ int read_adc(int channel){
   byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
 
   //allow channel selection
-  commandbits|=((channel-1)<<3);
+  commandbits|=((channel)<<3);
 
   digitalWrite(CS,LOW); //Select adc
   // setup bits to be written
   for (int i=7; i>=3; i--){
-    digitalWrite(MOSI,commandbits&1<<i);
+    digitalWrite(DI,commandbits&1<<i);
     //cycle clock
-    digitalWrite(CLK,HIGH);
-    digitalWrite(CLK,LOW);    
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);    
   }
 
-  digitalWrite(CLK,HIGH);    //ignores 2 null bits
-  digitalWrite(CLK,LOW);
-  digitalWrite(CLK,HIGH);  
-  digitalWrite(CLK,LOW);
+  digitalWrite(SPICLOCK,HIGH);    //ignores 2 null bits
+  digitalWrite(SPICLOCK,LOW);
+  digitalWrite(SPICLOCK,HIGH);  
+  digitalWrite(SPICLOCK,LOW);
 
   //read bits from adc
   for (int i=11; i>=0; i--){
-    adcvalue+=digitalRead(MISO)<<i;
+    adcvalue+=digitalRead(DO)<<i;
     //cycle clock
-    digitalWrite(CLK,HIGH);
-    digitalWrite(CLK,LOW);
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);
   }
   digitalWrite(CS, HIGH); //turn off device
   return adcvalue;
